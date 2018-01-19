ea6f56655ddb4cbadae7ac07da6bb1c9ed861232
Saomonite <981003163@qq.com>
2018-01-19 16:26:32 +0800

    web

---
+++ web/src/components/router.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router,Route} from 'react-router';
+
+class RootRouters extends React.Component{
+  render(){
+    return (
+      
+      
+        <div>init</div>
+      
+
+      
+    );
+  };
+}
+
+export default RootRouters;
---
+++ web/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RootRouters from './components/router'
+
+
+
+ReactDOM.render(<RootRouters />, document.getElementById('root'));
+
