a409c2a3d8c94e794e5e5b34ec93589b75e6f47a
Saomonite <981003163@qq.com>
2018-01-30 11:55:17 +0800

    save before ejecting

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addGUN, removeGUN, addGunAsync } from './index.redux'
+  class App extends Component {
+    render() {
+      return (
+        <div>
+          <h1>现在有机枪{this.props.num}把</h1>
+          <button onClick={this.props.addGUN}>申请武器</button>
+          <button onClick={this.props.removeGUN}>上交武器</button>
+          <button onClick={this.props.addGunAsync}>托两天上交武器</button>
+        </div>
+      )
+    }
+  }
+const mapStateToProps = (state) => {
+  return {
+    num: state
   }
 }
-
-export default App;
+const actionCreators = { addGUN, removeGUN, addGunAsync }
+App = connect(mapStateToProps, actionCreators)(App)
+export default App
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './app'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { counter } from './index.redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+
+const store = createStore(counter, compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+))
+
+
+ReactDom.render(
+  (<Provider store={store}>
+    <App />
+  </Provider>)
+  , document.getElementById('root')
+)
+
+
